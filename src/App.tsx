12c82bb0e16a0c7ab0013be5b72b7c10f5bc1a69
SiqueiraAna <keel_siqueira@hotmail.com>
2021-11-09 09:43:30 -0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-font-size: 64px;
-color:#8257e6`;
+const Title =styled.h1 `
+    font-size: 64px;
+    color: #8257e6;
+`
 
 export function App() {
-  return (
-    <div className="App">
-      <Title className="title">Hello</Title>
-    </div>
-  );
+    return (
+      <div className="App">
+        <Title>
+          Helloo
+        <button>Salvar</button>          
+        </Title>
+      </div>
+    );
 }
-
-
