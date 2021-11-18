154b86f3e1d1584d05fc23d3fae2d286acf2fdeb
SiqueiraAna <keel_siqueira@hotmail.com>
2021-11-18 16:37:40 -0300

    update

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,9 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
+import closeImg  from '../../assets/close.svg';
+
+import { Container, TransactionTypeContainer } from './styles';
 
 
 interface NewTransactionModalProps {
@@ -19,7 +23,16 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                 //função que deve ser executada quando o usuário pedi para fechar o modal.
                 overlayClassName="react-modal-overlay"
                 className="react-modal-content"
-                >       
+                >     
+
+                    <button 
+                        type="button" 
+                        onClick={onRequestClose} 
+                        className="react-modal-close"
+                        >
+                        <img src={closeImg} alt="Fechar modal" />
+                    </button>
+
                     <Container> 
 
                         <h2>Cadastrar transação</h2>
@@ -30,6 +43,23 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                             type="number"
                             placeholder="Valor" 
                         />
+
+                        <TransactionTypeContainer>
+                            <button 
+                                type="button"
+                            >
+                                <img src={incomeImg} alt="Entrada" />
+                                <span>Entrada</span>
+                            </button>
+
+                            <button 
+                                type="button"
+                            >
+                                <img src={outcomeImg} alt="Saída" />
+                                <span>Saída</span>
+                            </button>
+                        </TransactionTypeContainer>
+
                         <input 
                             placeholder="Categoria" 
                         />
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { darken } from 'polished';
 
 export const Container = styled.form`
     h2 {
@@ -29,7 +30,7 @@ export const Container = styled.form`
     }
 
     button[type="submit"] {
-        width: 100%;
+        width: 100%; 
         padding: 0 1.5rem;
         height: 4rem;
         background: var(--blue);
@@ -38,5 +39,48 @@ export const Container = styled.form`
         border: 0;
         font-size: 1rem;
         margin-top: 1.5rem;
+        font-weight: 600;
+
+        transition: filter 0.2s;  //transição quando passar o mouse encima do botão. 
+
+        &:hover {
+            filter: brightness(0.9);  
+        }
     }
 `;
+
+    export const TransactionTypeContainer =styled.div`
+        margin: 1rem 0;
+        display: grid;
+        grid-template-columns: 1fr 1fr;
+        gap: 0.5rem;
+        
+        button{
+            height: 4rem;
+            border: 1px solid #d7d7d7;
+            border-radius: 0.25rem;
+
+            background: transparent;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+
+            &:hover {
+              border-color: ${darken(0.1, '#d7d7d7')};
+            }
+
+            img {
+                width: 20px; //
+                height: 20px; //
+            }
+
+            span{
+                display: inline-block;
+                margin-top: 1rem;
+                font-size: 1rem;
+                color: var(--text-title);
+            }
+        }
+
+    `;
