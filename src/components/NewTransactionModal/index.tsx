c3584fa4fbcac9e89957785d01fce980fb51ebb4
SiqueiraAna <keel_siqueira@hotmail.com>
2021-11-23 14:40:09 -0300

    update

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
+import { FormEvent, useState } from 'react';
 import Modal from 'react-modal'; // importando o modal 
 import incomeImg from '../../assets/income.svg'; // Importando a img incomeImg
 import outcomeImg from '../../assets/outcome.svg'; // Importando a img  outcomeImg
 import closeImg  from '../../assets/close.svg'; // Importando a img closeImg 
-import { useState } from 'react';
+
 
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 // Importando o Container e TransactionTypeContainer
@@ -18,15 +19,18 @@ interface NewTransactionModalProps {
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps ) {
     //criação do estado 
+    const [title, setTitle] =useState('');
+    const [value, setValue] =useState(0);
+    const [category, setCategory] =useState('');
     const [type, setType] = useState('deposit');
+
     // type tipo da transação e o tipo de entrada (deposit)
 
-    function hadleCreateNewTransaction(){
-        
+    function hadleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
     }
    
 
-    
     return (
         <Modal 
                 isOpen={isOpen} //propriedade -> isOpen e o estado. 
@@ -49,10 +53,19 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
                         <h2>Cadastrar transação</h2>
                         <input 
                             placeholder="Títutlo" 
+                            value={title}
+                            onChange={event => setTitle(event.target.value)}
+                            // toda vez que acontece uma digitação no input 
+                            // aparecer uma troca de valor no input
+                            // salva o novo valor digitado dentro da propriedade setTitle
                         />
                         <input 
                             type="number"
-                            placeholder="Valor" 
+                            placeholder="Valor"
+                            value={value}
+                            onChange={event => setValue(Number(event.target.value))} 
+            
+                            
                         />
 
                         <TransactionTypeContainer>
